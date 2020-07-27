16a0f93abe9ed236d06a17ba2807033d6bc5df7a
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-27 17:27:28 -0500

    The use of objects to debug the operations was implemented.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,63 @@
 const defaultResult = 0
 let currentResult = defaultResult
+let logEntires = []
 
-function getUserNumberInput(){
+function getUserNumberInput() {
     return parseInt(userInput.value)
 }
 
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}` 
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
     outputResult(currentResult, calcDescription)
 }
 
-function add(){    
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult) {
+
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    }
+    logEntires.push(logEntry)
+    console.log(logEntires);
+}
+
+function add() {
     const enteredNumber = getUserNumberInput()
     const initalResult = currentResult
-    currentResult += enteredNumber    
+    currentResult += enteredNumber
     createAndWriteOutput('+', initalResult, enteredNumber)
+    writeToLog('ADD', initalResult, enteredNumber, currentResult)
 }
 
-function substract(){
+function substract() {
     const enteredNumber = getUserNumberInput()
     const initalResult = currentResult
-    currentResult -= enteredNumber    
+    currentResult -= enteredNumber
     createAndWriteOutput('-', initalResult, enteredNumber)
+    writeToLog('SUBSTRACT', initalResult, enteredNumber, currentResult)
 }
 
-function multiply(){
+function multiply() {
     const enteredNumber = getUserNumberInput()
     const initalResult = currentResult
-    currentResult *= enteredNumber    
+    currentResult *= enteredNumber
     createAndWriteOutput('*', initalResult, enteredNumber)
+    writeToLog('MULTIPLY', initalResult, enteredNumber, currentResult)
 }
 
-function divide(){
+function divide() {
     const enteredNumber = getUserNumberInput()
     const initalResult = currentResult
-    currentResult /= enteredNumber    
+    currentResult /= enteredNumber
     createAndWriteOutput('/', initalResult, enteredNumber)
+    writeToLog('DIVIDE', initalResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click', add)
