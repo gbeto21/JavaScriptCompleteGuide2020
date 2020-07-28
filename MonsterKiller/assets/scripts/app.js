7c3b2bdc69b1dfc0b7dc09ad5e0dce3721e1c932
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-28 11:12:26 -0500

    The funtionality of imput the value of the life was implemented.

--- MonsterKiller/assets/scripts/app.js
+++ MonsterKiller/assets/scripts/app.js
@@ -3,12 +3,24 @@ const MONSTER_ATTACT_VALUE = 14
 const STRONG_ATTACK_VALUE = 17
 const HEAL_VALUE = 20
 
-let chosenMaxLife = 100
+const enteredValue = prompt('Maximon life for you and the monster.', '100')
+
+let chosenMaxLife = parseInt(enteredValue)
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100
+}
+
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 let hasBonusLife = true
 
 adjustHealthBars(chosenMaxLife)
+
+function reset() {
+    currentMonsterHealth = chosenMaxLife
+    currentPlayerHealth = chosenMaxLife
+    resetGame(chosenMaxLife)
+}
 
 function endRound() {
     const initialPlayerHealth = currentPlayerHealth
@@ -25,12 +37,15 @@ function endRound() {
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won!')
+        reset()
     }
     else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You lost!')
+        reset()
     }
     else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert('You have a draw!')
+        reset()
     }
 }
 
