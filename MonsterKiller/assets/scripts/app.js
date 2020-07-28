500bd90480b809bd851578e3362ba3f6b6258062
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-28 10:09:00 -0500

    The funtionality of atack the monster was completed.

--- MonsterKiller/assets/scripts/app.js
+++ MonsterKiller/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACT_VALUE = 10
+const MONSTER_ATTACT_VALUE = 14
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -6,9 +7,22 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler(){
+function attackHandler() {
     const damage = dealMonsterDamage(ATTACT_VALUE)
     currentMonsterHealth -= damage
+
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACT_VALUE)
+    currentPlayerHealth -= playerDamage
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!')
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!')
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!')
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler)
