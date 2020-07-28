13ca2468014ba85eab1999314eea70444e0ad81c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-28 10:45:50 -0500

    The funtionality of recover helth was implemented.

--- MonsterKiller/assets/scripts/app.js
+++ MonsterKiller/assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACT_VALUE = 10
 const MONSTER_ATTACT_VALUE = 14
+const STRONG_ATTACK_VALUE = 17
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -7,10 +9,7 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACT_VALUE)
-    currentMonsterHealth -= damage
-
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACT_VALUE)
     currentPlayerHealth -= playerDamage
 
@@ -25,4 +24,40 @@ function attackHandler() {
     }
 }
 
+function attactMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACT') {
+        maxDamage = ATTACT_VALUE
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+
+    const damage = dealMonsterDamage(maxDamage)
+    currentMonsterHealth -= damage
+    endRound()
+}
+
+function attackHandler() {
+    attactMonster('ATTACT')
+}
+
+function stringAttackHandler() {
+    attactMonster('STRONG_ATTACK')
+}
+
+function healPlayerHandler() {
+    let healValue
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert("You can't heal to more that your max initial health.")
+        healValue = chosenMaxLife - currentPlayerHealth
+    }else{
+        healValue = HEAL_VALUE
+    }
+    increasePlayerHealth(healValue)
+    currentPlayerHealth += healValue
+    endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', stringAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
