a314f4f77753a3718556206b140eb56161be9462
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-29 07:30:31 -0500

    The use of switch statment was implemented.

--- MonsterKiller/assets/scripts/app.js
+++ MonsterKiller/assets/scripts/app.js
@@ -34,42 +34,88 @@ function writeToLong(event, value, monsterHealth, playerHealth) {
         finalPlayerHealth: playerHealth
     }
 
-    if (event === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry.target = 'MONSTER'
-    } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'MONSTER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        }
-    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'PLAYER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        }
+    switch (event) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER'
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            }
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            }
+            break;
+        case LOG_EVENT_MONSTER_PLAYER_HEAL:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            }
+            break;
+
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event: event,
+                value: value,
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            }
+            break;
+
+        default:
+            logEntry = {}
+            break;
     }
-    else if (event === LOG_EVENT_MONSTER_PLAYER_HEAL) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'PLAYER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        }
-    }
-    else if (event === LOG_EVENT_GAME_OVER) {
-        logEntry = {
-            event: event,
-            value: value,
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        }
-    }
+
+    // if (event === LOG_EVENT_PLAYER_ATTACK) {
+    //     logEntry.target = 'MONSTER'
+    // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'MONSTER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     }
+    // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'PLAYER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     }
+    // }
+    // else if (event === LOG_EVENT_MONSTER_PLAYER_HEAL) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'PLAYER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     }
+    // }
+    // else if (event === LOG_EVENT_GAME_OVER) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     }
+    // }
     battleLog.push(logEntry)
 }
 
