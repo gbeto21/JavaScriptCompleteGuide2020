e32e9c1d05b3c224a284daa8a5e4e489834c10f8
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-01 08:09:02 -0500

    Render movies after they were added, was implemented..

--- DOMProyect/assets/scripts/app.js
+++ DOMProyect/assets/scripts/app.js
@@ -4,6 +4,33 @@ const backdrop = document.getElementById('backdrop')
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
 const consfirmAddMovieButton = cancelAddMovieButton.nextElementSibling
 const userInputs = addMovieModal.querySelectorAll('input')
+const entryTextSection = document.getElementById('entry-text')
+const movies = []
+
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block'
+    }
+    else {
+        entryTextSection.style.display = 'none'
+    }
+}
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li')
+    newMovieElement.className = 'movie-element'
+    newMovieElement.innerHTML = `
+        <div class = "movie-element__image">
+            <img src="${imageUrl}" alt = "${title}">
+        </div>
+        <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating}/5 stars</p>
+        </div>
+    `
+    const listRoot = document.getElementById('movie-list')
+    listRoot.append(newMovieElement)
+}
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -20,21 +47,40 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieHandler = () => {
     toggleMovieModal()
+    clearMovieInputs()
+}
+
+const clearMovieInputs = () => {
+    for (const usrInput of userInputs) {
+        usrInput.value = ''
+    }
 }
 
 const addMovieHandler = () => {
     const titleValue = userInputs[0].value
-    const imageURL = userInputs[1].value
+    const imageURLValue = userInputs[1].value
     const ratingValue = userInputs[2].value
     if (
         titleValue.trim() === '' ||
-        imageURL.trim() === '' ||
+        imageURLValue.trim() === '' ||
         ratingValue.trim() === '' ||
         +ratingValue < 1 ||
         +ratingValue > 5
     ) {
         alert('Please enter valid values (rating between 1 and 5')
+        return
     }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageURLValue,
+        rating: ratingValue
+    }
+    movies.push(newMovie)
+    toggleMovieModal()
+    clearMovieInputs()
+    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating)
+    updateUI()
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
