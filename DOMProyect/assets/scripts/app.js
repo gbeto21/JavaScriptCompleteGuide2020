8d483f58638721bcf4115c861d080d0a933bf637
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-01 07:37:22 -0500

    The funtionality of validate data was implemented.

--- DOMProyect/assets/scripts/app.js
+++ DOMProyect/assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById('add-modal')
 const startAddMovieButton = document.querySelector('header button')
 const backdrop = document.getElementById('backdrop')
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+const consfirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll('input')
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -16,10 +18,26 @@ const backdropClickHandler = () => {
     toggleBackdrop()
 }
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal()
+}
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value
+    const imageURL = userInputs[1].value
+    const ratingValue = userInputs[2].value
+    if (
+        titleValue.trim() === '' ||
+        imageURL.trim() === '' ||
+        ratingValue.trim() === '' ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid values (rating between 1 and 5')
+    }
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', toggleMovieModal)
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler)
+consfirmAddMovieButton.addEventListener('click', addMovieHandler)
